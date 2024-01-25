377fecdde95c0ebf48ae850ced99634f1962b384
kkcoms <kkcoms@byui.edu>
2024-01-25 01:24:18 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Next.js & Whisper",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [],
+  links: {
+    twitter: "https://twitter.com/makrdev",
+    github: "https://github.com/batuhanbilginn",
+    docs: "https://ui.shadcn.com",
+  },
+}
